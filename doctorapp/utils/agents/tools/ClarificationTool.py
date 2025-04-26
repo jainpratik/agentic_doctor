d305cc6420e78346e2927b1adad148a06a28a0af
Pratik Jain <pratikjain@gmail.com>
2025-04-26 14:32:02 -0700

    Delete doctorapp directory

--- doctorapp/utils/agents/tools/ClarificationTool.py
+++
@@ -1,7 +0,0 @@
-from langchain.tools import Tool
-
-clarifier_tool = Tool(
-    name="Clarifier",
-    func=lambda q: "I need more information. Can you clarify what symptoms are most severe or when they started?",
-    description="Used when the input is ambiguous or lacks key clinical details.",
-)
